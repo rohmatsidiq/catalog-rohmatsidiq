74c713a92f8780495746b1fd36485d61ac36af3c
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2025-12-31 11:07:40 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ConfigProvider
       theme={{
         token: {
-          colorPrimary: "red",
+          colorPrimary: "#009689",
         },
       }}
     >
